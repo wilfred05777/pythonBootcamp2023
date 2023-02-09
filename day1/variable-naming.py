967e40b5d0bcdd2788d4096e8c695ceb8a621c29
Wilfred <wilfred05777@gmail.com>
2023-02-09 18:26:48 +0800

    day1

---
+++ day1/variable-naming.py
@@ -0,0 +1,6 @@
+# name = input("What is your name? \n")
+# length = len(name)
+# print(length)
+
+time_until_midnight = '5'
+print('There are ' + time_until_midnight + ' hours until midnight')
---
+++ day1/variables.py
@@ -0,0 +1,9 @@
+name = input('What is your name?')
+print(name)
+
+
+print()
+name2 = input("What is your name?")
+length = len(name2)
+
+print(length)
